610da78efe919421b8a1ac783cf31fb83d3e3b60
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2020-04-07 19:06:51 -0300

    Estilizando Reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
+
+import './style.css';
 
 function Reservas() {
   return(
     <div>
-      <h1>Reservas</h1>
+      <h1 className="title">Você soliticou 1 reservas</h1>
+      <div className="reservas">
+        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt="Maceió" />
+        <strong>Viagem Maceió 7 Dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#A7A7A7" />
+        </button>
+      </div>
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
     </div>
   )
 }
