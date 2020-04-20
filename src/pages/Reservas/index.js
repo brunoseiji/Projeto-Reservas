dd9d683d8d2af08fab348c7c21a19849979a72b7
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2020-04-19 21:09:54 -0300

    Mudando a quantidade

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { MdDelete } from 'react-icons/md';
+import { removeReserve, updateAmountReserve } from '../../store/modules/reserve/actions';
+import { MdDelete, MdAddCircle, MdRemoveCircle } from 'react-icons/md';
 import './style.css';
-import { removeReserve } from '../../store/modules/reserve/actions';
+
 
 function Reservas() {
 
@@ -13,14 +14,35 @@ function Reservas() {
     dispatch(removeReserve(id))
   }
 
+  function decrementAmount(trip) {
+    dispatch(updateAmountReserve(trip.id, trip.amount - 1));
+  }
+
+  function incrementAmount(trip) {
+    dispatch(updateAmountReserve(trip.id, trip.amount + 1));
+  }
+
   return(
     <div>
       <h1 className="title">Você soliticou {reserves.length} reservas</h1>
+
       {reserves.map(reserve => (
         <div className="reservas" key={reserve.id}>
           <img src={reserve.image} alt={reserve.title} />
           <strong>{reserve.title}</strong>
-          <span>Quantidade: {reserve.amount}</span>
+          <div id="amount">
+
+            <button type="button" onClick={() => decrementAmount(reserve)}>
+              <MdRemoveCircle size={25} color="#A7A7A7" />
+            </button>
+
+            <input type="text" readOnly value={reserve.amount} />
+
+            <button type="button" onClick={() => incrementAmount(reserve)}>
+              <MdAddCircle size={25} color="#A7A7A7" />
+            </button>
+
+          </div>
           <button type="button" onClick={() => handleRemove(reserve.id)}>
             <MdDelete size={20} color="#A7A7A7" />
           </button>
--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -3,6 +3,7 @@ import produce from 'immer';
 export default function reserve(state = [], action ) {
     
     switch(action.type) {
+
         case 'ADD_RESERVE':
             return produce(state, draft => {
 
@@ -17,6 +18,7 @@ export default function reserve(state = [], action ) {
                 });
             }
         });
+
         case 'REMOVE_RESERVE':
             return produce(state, draft => {
                 const tripIndex = draft.findIndex(trip => trip.id === action.id);
@@ -25,6 +27,21 @@ export default function reserve(state = [], action ) {
                     draft.splice(tripIndex, 1);
                 }
             });
+
+        case 'UPDATE_RESERVE': {
+
+            if(action.amount <= 0) {
+                return state;
+            }
+
+            return produce(state, draft => {
+                const tripIndex = draft.findIndex(trip => trip.id === action.id);
+
+                if(tripIndex >= 0) {
+                    draft[tripIndex].amount = Number(action.amount);
+                }
+            });
+        }
         default: 
             return state;
     }
